cb1d4cf23fe7796291c8c91fbf39ee70fad05438
Henrique Colini <henriquecolini@gmail.com>
2023-10-25 20:27:17 -0300

    Fix split char by char

--- src/runtime/output.rs
+++ src/runtime/output.rs
@@ -5,8 +5,23 @@ pub struct Output {
 }
 
 pub enum OutputSplitIterator<'a> {
+	SplitChars(CharIterator<'a>),
 	Split(std::str::Split<'a, &'a str>),
     SplitWhitespace(std::str::SplitWhitespace<'a>),
+}
+
+pub struct CharIterator<'a> {
+    input: &'a str,
+    index: usize,
+}
+
+impl<'a> CharIterator<'a> {
+    fn new(input: &'a str) -> CharIterator<'a> {
+        CharIterator {
+            input,
+            index: 0,
+        }
+    }
 }
 
 pub fn join_outputs(outputs: &[Output]) -> String {
@@ -45,7 +60,13 @@ impl Output {
 	}
 	pub fn split_iter<'a>(&'a self, arg: Option<&'a Output>) -> OutputSplitIterator<'a> {
 		match arg {
-			Some(arg) => OutputSplitIterator::Split(self.value.split(&arg.value)),
+			Some(arg) => {
+				if arg.value == "" {
+					OutputSplitIterator::SplitChars(CharIterator::new(&self.value))
+				} else { 
+					OutputSplitIterator::Split(self.value.split(&arg.value))
+				}
+			},
 			None => OutputSplitIterator::SplitWhitespace(self.value.split_whitespace()),
 		}
 	}
@@ -59,8 +80,31 @@ impl<'a> Iterator for OutputSplitIterator<'a> {
 
 	fn next(&mut self) -> Option<Self::Item> {
 		match self {
+			OutputSplitIterator::SplitChars(iter) => iter.next(),
 			OutputSplitIterator::Split(iter) => iter.next(),
 			OutputSplitIterator::SplitWhitespace(iter) => iter.next(),
 		}
 	}
+}
+
+
+impl<'a> Iterator for CharIterator<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.input.len() {
+            None
+        } else {
+            let start = self.index;
+            let end = self.input[self.index..]
+                .char_indices()
+                .nth(1)
+                .map(|(i, _)| self.index + i)
+                .unwrap_or_else(|| self.input.len());
+
+            self.index = end;
+
+            Some(&self.input[start..end])
+        }
+    }
 }
